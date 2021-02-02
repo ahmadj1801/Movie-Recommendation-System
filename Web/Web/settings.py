b0a12a8ba3776cde0f1cf959e7028b793f75ca3b
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2021-02-02 21:36:10 +0200

    update settings.py - added db connectivity

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movies',
+        'USER': 'root',
+        'PASSWORD': 'mySql3000root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
