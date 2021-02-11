efe6e9268bab9ce1476628548f54f0cb27b3def0
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2021-02-11 15:23:06 +0200

    Test Gui form

--- Application/Application.py
+++ Application/Application.py
@@ -3,7 +3,10 @@ import tkinter
 # Create Window
 window = tkinter.Tk()
 # Window Title
-window.title("Movie Recommendation System")
+window.title("Movie System")
+# Window Size
+window.geometry('350x350')
 lbl_intro = tkinter.Label(window, text='Welcome '
-                                       'to the Movie Recommendation System!').pack()
+                                       'to the Movie Recommendation System!',
+                          font=("Arial Bold", 10)).grid(column=1, row=1)
 window.mainloop()
