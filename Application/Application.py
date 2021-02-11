776f0cfbf6ef8fba9f6c659e0575d165090f467b
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2021-02-11 14:53:08 +0200

    Test Gui form

---
+++ Application/Application.py
@@ -0,0 +1,9 @@
+import tkinter
+
+# Create Window
+window = tkinter.Tk()
+# Window Title
+window.title("Movie Recommendation System")
+lbl_intro = tkinter.Label(window, text='Welcome '
+                                       'to the Movie Recommendation System!').pack()
+window.mainloop()
